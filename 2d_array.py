56b3b3730b27a55922bff7cba6504779c1c487bd
Arif Absar <arifabsar182@gmail.com>
2025-04-11 12:17:11 +0600

    initial

---
+++ 2d_array.py
@@ -0,0 +1,12 @@
+import math
+import os
+import random
+import re
+import sys
+from math import inf
+arr=[]
+for row in range(0,3):
+  for col in range(0,3):
+    arr.append([row,col]).rstrip()
+
+print(arr)
